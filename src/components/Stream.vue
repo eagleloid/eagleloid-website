518cbfdb403083a268f96fef84fcd755e52d6be5
Eagle Loid <eagleyelord@gmail.com>
2020-09-19 02:02:07 -0500

    force take stream stuff
    because github...trying it one more time

--- src/components/Stream.vue
+++ src/components/Stream.vue
@@ -1,13 +1,30 @@
 <template>
-         width="560" 
-         height="315" 
-         src="https://www.youtube.com/embed/acmdtnFhbCQ" 
-         frameborder="0" 
-         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-         allowfullscreen></iframe>
-   </v-row>
-      <p class="text-center">This page is a work in progress. Excuse the mess.</p>
-      <p class="text-center">You can also go to any of the streaming services below for the same live content.</p>
-
-</v-container>
+   <v-container fluid>
+      <v-row 
+         justify="center"
+         align="end"
+         >
+            <div id="twitch-embed"></div>
+      </v-row>
+   </v-container>
 </template>
+<script>      
+  export default {
+            mounted() {
+               // get twitch js stuff
+               let externalScript = document.createElement('script')
+               externalScript.setAttribute('src', 'https://embed.twitch.tv/embed/v1.js')
+               // append it.
+               document.head.appendChild(externalScript)
+               // put the js code inside a script
+               externalScript = document.createElement('script')
+               externalScript.setAttribute('type', 'text/javascript')
+               let inlineScript = document.createTextNode('new Twitch.Embed("twitch-embed", {width: 854,height: 480,channel: "eagleloid",parent: ["eagleloid.com"]});');
+               // add the code to the script...
+               externalScript.appendChild(inlineScript)
+               // add the script to the site
+               document.head.appendChild(externalScript)
+            }
+  }
+</script>
+ 
