c4a0c2cbae36acbe2a13f5dde249197ce6b94649
eagleloid <eagleyelord@gmail.com>
2020-12-07 18:47:20 -0600

    :construction: Adding Server Status
    Pages will show current status of Eagle's gaming servers

---
+++ src/components/ServerSE.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+      <v-row >
+        <v-col>
+          <v-img
+          :src="require('../assets/download.png')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+        </v-col>
+        
+      </v-row>
+
+      <v-row >
+        <v-col>
+          <h1 class="text-center display-2 font-weight-bold mb-3">
+          Current Status of "The Nest's Gaming Server"
+          </h1>  
+        </v-col>
+        
+      </v-row>
+  </v-container>
+</template>
+
+<script>  
+// TODO: put the logic here that checks the game server's basic stats.
+// will probably need in import the third party solution.
+  export default {
+    data: () => ({
+      
+    })
+  }
+  
+</script>
---
+++ src/views/ServerStatus.vue
@@ -0,0 +1,15 @@
+<template>
+  <ServerSE />
+</template>
+
+<script>
+  import ServerSE from '../components/ServerSE'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      ServerSE,
+    },
+  }
+</script>
