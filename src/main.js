b88adac81349a1d6fa6019aff9bda7e7baa7a366
eagleloid <eagleyelord@gmail.com>
2021-01-29 18:34:40 -0600

    import store

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import './registerServiceWorker'
+import store from './store'
 
 Vue.config.productionTip = false
 
